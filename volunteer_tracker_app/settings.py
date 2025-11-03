e63b4b2ea98504c7fc0833a3089520de94544380
Cody Casteel <ccastee@iu.edu>
2025-11-03 14:52:25 -0500

    Update

--- volunteer_tracker_app/settings.py
+++ volunteer_tracker_app/settings.py
@@ -179,5 +179,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # used in production
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
