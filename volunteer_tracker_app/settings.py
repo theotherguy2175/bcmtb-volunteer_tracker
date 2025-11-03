05c0a5079ea6207077fb7d5a8495dc5a7ebea960
Cody Casteel <ccastee@iu.edu>
2025-11-03 12:31:06 -0500

    Updated Allow List

--- volunteer_tracker_app/settings.py
+++ volunteer_tracker_app/settings.py
@@ -112,7 +112,11 @@ DATABASES = {
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret')
 DEBUG = os.environ.get('DJANGO_DEBUG', '0') == '1'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
+else:
+    ALLOWED_HOSTS = ["*"]
 
 
 
