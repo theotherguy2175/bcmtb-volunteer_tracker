ba76466a2e05bff1f78a4a2ee2bc7526a49959d1
Cody Casteel <ccastee@iu.edu>
2025-12-23 12:09:16 -0500

    Added Site Icon Feature

--- volunteer_tracker_app/urls.py
+++ volunteer_tracker_app/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/bcmb_logo.ico')),
     path('admin/', admin.site.urls),
     path('', include('hourTracker.urls')),  # main app
     path('accounts/', include('django.contrib.auth.urls')),  # login/logout
